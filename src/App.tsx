9ff91c461d11b7f8f1da8e6480da2563cd29232b
Radosław Siek <Borderlandsmaniak@gmail.com>
2024-03-12 16:19:55 +0100

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import { BrowserRouter as Router } from 'react-router-dom';
-import Heading from './components/layout/Heading';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Explore from './pages/Explore/Explore';
+import Offers from './pages/Offers/Offers';
+import SignIn from './pages/SignIn/SignIn';
+import SignUp from './pages/SignUp/SignUp';
+import ForgotPassword from './pages/ForgotPassword/ForgotPassword';
+import NotFound from './pages/NotFound/NotFound';
 
 function App() {
   return (
     <div className="min-h-screen bg-neutral">
-      <Heading />
+      <Routes>
+        <Route path="/" element={<Explore />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/profile" element={<SignIn />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/notfound" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
